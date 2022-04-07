9caa77828453372d226f4999b2ca674ae8c9371e
tsd03150 <tsd03150@naver.com>
2022-04-07 10:36:35 +0900

    220417_2565_1 commit

---
+++ algorithm/src/com/boj_2565/Main.java
@@ -0,0 +1,40 @@
+package com.boj_2565;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = kb.nextInt();
+            arr[i][1] = kb.nextInt();
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        int[] dp = new int[n];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i][1] > arr[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(n - max);
+    }
+}
